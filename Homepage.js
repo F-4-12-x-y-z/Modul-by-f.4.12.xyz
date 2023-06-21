51fc993060b3627ba78597be6dd5be56a799715d
F.4.12.xyz <110985677+F-4-12-x-y-z@users.noreply.github.com>
2023-06-21 20:33:42 +0700

    Add files via upload

---
+++ Homepage.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Siapa namamu?");
+      if (firstName == "" || firstName == null) {
+        alert("Hmm you are so misterius.....");
+      } else {
+        alert("Hi " + firstName + ", Silahkan pilih mata pelajaran yangdiinginkan... :)");}
